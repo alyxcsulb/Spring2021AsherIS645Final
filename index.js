53e3e8dd859d48ae145a4125a2329a4f669a13b2
Alyx Faber <alyx.faber@student.csulb.edu>
2021-05-11 21:31:21 -0700

    partial commit

--- index.js
+++ index.js
@@ -2,8 +2,28 @@
 const express = require("express");
 const app = express();
 
+// Add addiional packages
+const dblib = require("./dblib.js");
+require('dotenv').config()
+
+const multer = require("multer");
+const upload = multer();
+
+// Add middleware to parse default urlencoded form
+app.use(express.urlencoded({ extended: false }));
+
 // Set up EJS
 app.set("view engine", "ejs");
+
+// Enable CORS (see https://enable-cors.org/server_expressjs.html)
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
 
 
 // Start listener
@@ -25,3 +45,63 @@ const pool = new Pool({
     rejectUnauthorized: false
   }
 });
+
+// get and post home
+
+
+// get and post sum of series
+app.get("/sum", (req, res) => {
+    res.render("sum");
+})
+
+app.post("/sum", async (req, res) => {
+    res.render("sum");
+})
+
+
+// get and post import aka input
+app.get("/input", async (req, res) => {
+    const totRecs = await dblib.getTotalRecords();
+    const book = {
+  
+      book_id: "",
+      title: "",
+      total_pages: "",
+      rating: "",
+      isbn: "",
+      published_date: ""
+  
+    };
+  
+    res.render("input", {
+      type: "get",
+      totRecs: totRecs.totRecords,
+      book: book
+    });
+  });
+ 
+ app.post("/input",  upload.single('filename'), (req, res) => {
+     if(!req.file || Object.keys(req.file).length === 0) {
+         message = "Error: Import file not uploaded";
+         return res.send(message);
+     };
+     //Read file line by line, inserting records
+     const buffer = req.file.buffer; 
+     const lines = buffer.toString().split(/\r?\n/);
+ 
+     lines.forEach(line => {
+          //console.log(line);
+          book = line.split(",");
+          //console.log(customer);
+          const sql = "INSERT INTO book(book_id, title, total_pages, rating, isbn, published_date) VALUES ($1, $2, $3, $4, $5, $6)";
+          pool.query(sql, book, (err, result) => {
+              if (err) {
+                  console.log(`Insert Error.  Error message: ${err.message}`);
+              } else {
+                  console.log(`Inserted successfully`);
+              }
+         });
+     });
+     message = `Processing Complete - Processed ${lines.length} records`;
+     res.send(message);
+ });

ee4e9d7107ce591cd89f98ecfb3590d95f7018ae
Alyx Faber <alyx.faber@student.csulb.edu>
2021-05-11 19:19:23 -0700

    commit

---
+++ dblib.js
@@ -0,0 +1,14 @@
+// required module
+require('dotenv').config()
+
+// Add database package and connection string (can remove ssl)
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+
+
---
+++ index.js
@@ -0,0 +1,27 @@
+// Add required packages
+const express = require("express");
+const app = express();
+
+// Set up EJS
+app.set("view engine", "ejs");
+
+
+// Start listener
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server started (http://localhost:3000/) !");
+});
+
+// Setup routes
+app.get("/", (req, res) => {
+    //res.send ("Hello world...");
+    res.render("index");
+});
+
+// Add database package and connection string (can remove ssl)
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
